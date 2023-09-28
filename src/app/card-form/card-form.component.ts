ee49e53fb08297bcc62e9ab131867dd382ba435c
Connie Leung <cleung334@gmail.com>
2023-09-28 23:59:10 +0800

    feat: create cardForm component

---
+++ src/app/card-form/card-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-card-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './card-form.component.html',
+  styleUrls: ['./card-form.component.scss']
+})
+export class CardFormComponent {
+
+}
